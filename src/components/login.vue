3d0e6bff33c160a4b093312dcaffc9edad2a6aa6
changlingling <changlingling@zuoyebang.com>
2020-04-07 19:53:27 +0800

    完成登陆

---
+++ src/components/login.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="login-container">
+    <div class="login-box">
+      <!-- 头像 -->
+      <div class="avatar">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        label-width="0px"
+        class="login-form"
+        :rules="loginFormRules"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+            type="password"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 这是表单的验证规则对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 登陆
+    login () {
+      // console.log(this)
+      this.$refs.loginFormRef.validate(async valid => {
+        // console.log(valid)
+        if (!valid) return
+        const { data: res } = await this.$http.post('/login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登陆失败')
+        this.$message.success('登陆成功！')
+        window.sessionStorage.setItem('token', res.data.token)
+        // 跳转到主页
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  position: relative;
+  height: 100%;
+  background-color: #2b4b6b;
+  //   登陆盒子
+  .login-box {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    //用户头像
+    .avatar {
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      box-sizing: border-box;
+      width: 130px;
+      height: 130px;
+      border-radius: 50%;
+      border: 1px solid #eeeeee;
+      padding: 10px;
+      background-color: #fff;
+      box-shadow: 0 0 10px #dddddd;
+    }
+    .avatar img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+    .login-form {
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      padding: 0 20px;
+      box-sizing: border-box;
+      .el-form-item {
+        margin-bottom: 20px;
+      }
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
+import '../src/assets/global.css'
+// 字体图标
+import '../src/assets/fonts/iconfont.css'
+import './plugins/element.js'
+import axios from 'axios'
 
+// 请求地址
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
