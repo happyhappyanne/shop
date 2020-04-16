e74a8987b1bd6a0c9149083abba9e6d51829ce7e
changlingling <changlingling@zuoyebang.com>
2020-04-16 23:31:36 +0800

    完成权限列表

--- src/components/roles/roles.vue
+++ src/components/roles/roles.vue
@@ -11,86 +11,82 @@
     <el-card>
       <el-row>
         <el-col :span="1">
-          <el-button type="primary" @click="dialogVisible = true"
-            >添加角色</el-button
-          >
+          <el-button type="primary"
+                     @click="dialogVisible = true">添加角色</el-button>
         </el-col>
       </el-row>
       <!-- 角色列表区域 -->
-      <el-table :data="rolesList" stripe style="width: 100%" border>
+      <el-table :data="rolesList"
+                stripe
+                style="width: 100%"
+                border>
         <el-table-column type="expand">
           <template slot-scope="scope">
-            <el-row
-              v-for="(item, i1) in scope.row.children"
-              :key="item.id"
-              :class="['bd-bottom', i1 === 0 ? 'db-top' : '', 'v-center']"
-            >
+            <el-row v-for="(item, i1) in scope.row.children"
+                    :key="item.id"
+                    :class="['bd-bottom', i1 === 0 ? 'db-top' : '', 'v-center']">
               <!-- 一级权限 -->
               <el-col :span="5">
-                <el-tag closable @close="removeRight(scope.row.id, item.id)">
+                <el-tag closable
+                        @close="removeRight(scope.row.id, item.id)">
                   {{ item.authName }}
                 </el-tag>
               </el-col>
               <!-- 二级三级权限 -->
               <el-col :span="19">
-                <el-row
-                  v-for="(subItem, i2) in item.children"
-                  :key="subItem.id"
-                  :class="['v-center', i2 === 0 ? '' : 'bd-top']"
-                >
+                <el-row v-for="(subItem, i2) in item.children"
+                        :key="subItem.id"
+                        :class="['v-center', i2 === 0 ? '' : 'bd-top']">
                   <el-col :span="6">
-                    <el-tag
-                      closable
-                      type="success"
-                      @close="removeRight(scope.row.id, subItem.id)"
-                      >{{ subItem.authName }}</el-tag
-                    >
+                    <el-tag closable
+                            type="success"
+                            @close="removeRight(scope.row.id, subItem.id)">{{ subItem.authName }}</el-tag>
                   </el-col>
                   <el-col :span="18">
-                    <el-tag
-                      type="warning"
-                      closable
-                      v-for="third in subItem.children"
-                      :key="third.id"
-                      @close="removeRight(scope.row.id, third.id)"
-                      >{{ third.authName }}</el-tag
-                    >
+                    <el-tag type="warning"
+                            closable
+                            v-for="third in subItem.children"
+                            :key="third.id"
+                            @close="removeRight(scope.row.id, third.id)">{{ third.authName }}</el-tag>
                   </el-col>
                 </el-row>
               </el-col>
             </el-row>
           </template>
         </el-table-column>
-        <el-table-column type="index" label="#"> </el-table-column>
-        <el-table-column prop="roleName" label="角色名称"> </el-table-column>
-        <el-table-column prop="roleDesc" label="角色描述"> </el-table-column>
-        <el-table-column label="操作" width="300">
+        <el-table-column type="index"
+                         label="#"> </el-table-column>
+        <el-table-column prop="roleName"
+                         label="角色名称"> </el-table-column>
+        <el-table-column prop="roleDesc"
+                         label="角色描述"> </el-table-column>
+        <el-table-column label="操作"
+                         width="300">
           <template slot-scope="scope">
-            <el-button type="primary" icon="el-icon-edit" size="mini"
-              >编辑</el-button
-            >
-            <el-button
-              type="danger"
-              icon="el-icon-delete"
-              size="mini"
-              @click="deleteRole(scope.row.id)"
-              >删除</el-button
-            >
-            <el-button type="warning" icon="el-icon-s-tools" size="mini"
-              >分配权限</el-button
-            >
+            <el-button type="primary"
+                       icon="el-icon-edit"
+                       size="mini"
+                       @click="showEditDialog(scope.row)">编辑</el-button>
+            <el-button type="danger"
+                       icon="el-icon-delete"
+                       size="mini"
+                       @click="deleteRole(scope.row.id)">删除</el-button>
+            <el-button type="warning"
+                       icon="el-icon-s-tools"
+                       size="mini"
+                       @click="roleTreeVisible=true">分配权限</el-button>
           </template>
         </el-table-column>
       </el-table>
     </el-card>
 
     <!-- 添加用户弹窗 -->
-    <el-dialog
-      title="添加角色"
-      :visible.sync="dialogVisible"
-      @close="resetField"
-    >
-      <el-form ref="addRole" :model="role" label-width="80px">
+    <el-dialog title="添加角色"
+               :visible.sync="dialogVisible"
+               @close="resetField">
+      <el-form ref="addRole"
+               :model="role"
+               label-width="80px">
         <el-form-item label="角色名称">
           <el-input v-model="role.roleName"></el-input>
         </el-form-item>
@@ -98,31 +94,84 @@
           <el-input v-model="role.roleDesc"></el-input>
         </el-form-item>
       </el-form>
-      <span slot="footer" class="dialog-footer">
+      <span slot="footer"
+            class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addRole">确 定</el-button>
+        <el-button type="primary"
+                   @click="addRole">确 定</el-button>
       </span>
     </el-dialog>
+
+    <!-- 编辑角色对话框 -->
+    <el-dialog title="编辑角色"
+               :visible.sync="editeditDialogVisible"
+               @close="resetEditField">
+      <el-form ref="editRole"
+               :model="role"
+               label-width="80px">
+        <el-form-item label="角色名称">
+          <el-input v-model="role.roleName"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述">
+          <el-input v-model="role.roleDesc"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer"
+            class="dialog-footer">
+        <el-button @click="editeditDialogVisible = false">取 消</el-button>
+        <el-button type="primary"
+                   @click="editRole()">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <!-- 分配权限树形控件 -->
+    <el-dialog title="分配权限"
+               :visible.sync="roleTreeVisible">
+      <!-- 树形控件 -->
+      <el-tree :data="rolesList"
+               show-checkbox
+               node-key="id"
+               :default-expanded-keys="[2, 3]"
+               :default-checked-keys="[5]"
+               :props="defaultProps">
+      </el-tree>
+      <span slot="footer"
+            class="dialog-footer">
+        <el-button @click=" roleTreeVisible = false">取 消</el-button>
+        <el-button type="primary"
+                   @click="roleTreeVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+
   </div>
 </template>
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       rolesList: [],
       // 添加用户弹窗
       dialogVisible: false,
+      // 编辑用户弹窗
+      editeditDialogVisible: false,
       // 添加用户
       role: {
         roleName: '',
         roleDesc: ''
-      }
+      },
+      // 权限树形控件配置
+      defaultProps: {
+        label: 'authName',
+        children: 'children'
+      },
+      // 权限树形控件配置 显示状态
+      roleTreeVisible: false
     }
   },
   methods: {
     // 获取角色列表
-    async getRolesList() {
+    async getRolesList () {
       const { data: res } = await this.$http.get('roles')
       if (res.meta.status !== 200) {
         return this.$message.error('获取角色列表失败')
@@ -130,7 +179,7 @@ export default {
       this.rolesList = res.data
     },
     // 删除角色
-    async deleteRole(id) {
+    async deleteRole (id) {
       console.log(id)
       const { data: res } = await this.$http.delete(`roles/${id}`)
       if (res.meta.status !== 200) return this.$message.error('删除角色失败')
@@ -138,7 +187,7 @@ export default {
       this.getRolesList()
     },
     // 添加角色
-    async addRole() {
+    async addRole () {
       this.dialogVisible = false
       const { data: res } = await this.$http.post('roles', this.role)
       if (res.meta.status !== 201) return this.$message.error('添加角色失败')
@@ -147,12 +196,12 @@ export default {
       this.resetFields()
     },
     // 重置添加角色表单
-    resetField() {
-      console.log(this.$refs.addRole.resetFields())
+    resetField () {
+      // console.log(this.$refs.addRole.resetFields())
       this.$refs.addRole.resetFields()
     },
     // 删除权限
-    async removeRight(roleId, rightId) {
+    async removeRight (roleId, rightId) {
       const { data: res } = await this.$http.delete(
         `roles/${roleId}/rights/${rightId}`
       )
@@ -162,9 +211,31 @@ export default {
       }
       this.$message.success('删除角色权限成功')
       this.getRolesList()
+    },
+    // 编辑角色
+    async editRole () {
+      // console.log(this.role.id)
+      const { data: res } = await this.$http.put(
+        `roles/${this.role.id}`,
+        this.role
+      )
+      if (res.meta.status !== 200) return this.$message.error('编辑角色失败')
+      this.$message.success('编辑角色成功')
+      this.editeditDialogVisible = false
+      this.resetEditField()
+      this.getRolesList()
+    },
+    // 显示编辑对话框
+    showEditDialog (role) {
+      this.editeditDialogVisible = true
+      this.role = role
+    },
+    // 重置编辑弹窗
+    resetEditField () {
+      this.$refs.editRole.resetFields()
     }
   },
-  created() {
+  created () {
     this.getRolesList()
   }
 }
--- src/components/user/user.vue
+++ src/components/user/user.vue
@@ -11,101 +11,104 @@
       <el-row :gutter="20">
         <el-col :span="8">
           <!--  搜索框 -->
-          <el-input placeholder="请输入内容" v-model="queryInfo.query">
-            <el-button
-              slot="append"
-              icon="el-icon-search"
-              @click="getUserList"
-            ></el-button>
+          <el-input placeholder="请输入内容"
+                    v-model="queryInfo.query">
+            <el-button slot="append"
+                       icon="el-icon-search"
+                       @click="getUserList"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
-          <el-button type="primary" @click="dialogVisible = true"
-            >添加用户</el-button
-          >
+          <el-button type="primary"
+                     @click="dialogVisible = true">添加用户</el-button>
         </el-col>
       </el-row>
 
       <!-- 用户列表区域 -->
-      <el-table :data="userList" stripe border>
-        <el-table-column type="index" label="#"> </el-table-column>
-        <el-table-column prop="username" label="姓名"> </el-table-column>
-        <el-table-column prop="mobile" label="电话"> </el-table-column>
-        <el-table-column prop="email" label="邮箱"> </el-table-column>
-        <el-table-column prop="role_name" label="角色"> </el-table-column>
-        <el-table-column prop="mg_state" label="状态">
+      <el-table :data="userList"
+                stripe
+                border>
+        <el-table-column type="index"
+                         label="#"> </el-table-column>
+        <el-table-column prop="username"
+                         label="姓名"> </el-table-column>
+        <el-table-column prop="mobile"
+                         label="电话"> </el-table-column>
+        <el-table-column prop="email"
+                         label="邮箱"> </el-table-column>
+        <el-table-column prop="role_name"
+                         label="角色"> </el-table-column>
+        <el-table-column prop="mg_state"
+                         label="状态">
           <template slot-scope="scope">
-            <el-switch
-              v-model="scope.row.mg_state"
-              @change="userStateChange(scope.row)"
-            >
+            <el-switch v-model="scope.row.mg_state"
+                       @change="userStateChange(scope.row)">
             </el-switch>
           </template>
         </el-table-column>
-        <el-table-column label="操作" width="180">
-          <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
-          <el-button
-            type="danger"
-            icon="el-icon-delete"
-            size="mini"
-          ></el-button>
-          <el-button
-            type="warning"
-            icon="el-icon-s-tools"
-            size="mini"
-          ></el-button>
+        <el-table-column label="操作"
+                         width="180">
+          <el-button type="primary"
+                     icon="el-icon-edit"
+                     size="mini"></el-button>
+          <el-button type="danger"
+                     icon="el-icon-delete"
+                     size="mini"></el-button>
+          <el-button type="warning"
+                     icon="el-icon-s-tools"
+                     size="mini"></el-button>
         </el-table-column>
       </el-table>
 
       <!-- 分页区域 -->
       <el-row>
         <el-col :span="8">
-          <el-pagination
-            @size-change="handleSizeChange"
-            @current-change="handleCurrentChange"
-            :current-page="queryInfo.pagenum"
-            :page-sizes="[1, 2, 5, 10]"
-            :page-size="queryInfo.pagesize"
-            layout="total, sizes, prev, pager, next, jumper"
-            :total="totalpage"
-          >
+          <el-pagination @size-change="handleSizeChange"
+                         @current-change="handleCurrentChange"
+                         :current-page="queryInfo.pagenum"
+                         :page-sizes="[1, 2, 5, 10]"
+                         :page-size="queryInfo.pagesize"
+                         layout="total, sizes, prev, pager, next, jumper"
+                         :total="totalpage">
           </el-pagination>
         </el-col>
       </el-row>
     </el-card>
 
     <!-- 添加用户对话框 -->
-    <el-dialog
-      title="添加用户"
-      :visible.sync="dialogVisible"
-      width="30%"
-      @close="reSetForm"
-    >
-      <el-form
-        :model="userInfo"
-        :rules="rules"
-        ref="ruleForm"
-        label-width="60px"
-        :hide-required-asterisk="true"
-        class="demo-ruleForm"
-      >
-        <el-form-item label="用户名" prop="username">
+    <el-dialog title="添加用户"
+               :visible.sync="dialogVisible"
+               width="30%"
+               @close="reSetForm">
+      <el-form :model="userInfo"
+               :rules="rules"
+               ref="ruleForm"
+               label-width="60px"
+               :hide-required-asterisk="true"
+               class="demo-ruleForm">
+        <el-form-item label="用户名"
+                      prop="username">
           <el-input v-model="userInfo.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码" prop="password">
+        <el-form-item label="密码"
+                      prop="password">
           <el-input v-model="userInfo.password"></el-input>
         </el-form-item>
-        <el-form-item label="邮箱" prop="email">
+        <el-form-item label="邮箱"
+                      prop="email">
           <el-input v-model="userInfo.email"></el-input>
         </el-form-item>
 
-        <el-form-item label="手机号" prop="mobile">
+        <el-form-item label="手机号"
+                      prop="mobile">
           <el-input v-model="userInfo.mobile"></el-input>
         </el-form-item>
       </el-form>
-      <span slot="footer" class="dialog-footer">
+      <span slot="footer"
+            class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addUser">确 定</el-button>
+        <el-button type="primary"
+                   @click="addUser">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -113,28 +116,28 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       // 搜索框
       queryInfo: {
         query: '',
-        pagenum: 1, //当前页码
-        pagesize: 1 //每页显示数量
+        pagenum: 1, // 当前页码
+        pagesize: 10 // 每页显示数量
       },
-      //用户列表
+      // 用户列表
       userList: [],
-      //总记录数
+      // 总记录数
       totalpage: 0,
-      //添加用户对话框显示状态
+      // 添加用户对话框显示状态
       dialogVisible: false,
-      //用户信息
+      // 用户信息
       userInfo: {
         username: '',
         password: '',
         email: '',
         mobile: ''
       },
-      //添加用户校验规则
+      // 添加用户校验规则
       rules: {
         username: [
           { required: true, message: '用户名不能为空', trigger: 'blur' },
@@ -150,58 +153,56 @@ export default {
     }
   },
   methods: {
-    //获取用户列表
-    async getUserList() {
+    // 获取用户列表
+    async getUserList () {
       const { data: res } = await this.$http.get('users', {
         params: this.queryInfo
       })
       console.log(res)
-      if (res.meta.status !== 200)
-        return this.$message.error('获取用户列表失败')
+      if (res.meta.status !== 200) { return this.$message.error('获取用户列表失败') }
       this.userList = res.data.users
       this.totalpage = res.data.total
     },
-    //更改页面数量
-    handleSizeChange(newPageSize) {
+    // 更改页面数量
+    handleSizeChange (newPageSize) {
       this.queryInfo.pagesize = newPageSize
-      //console.log(newPageSize)
+      // console.log(newPageSize)
       this.getUserList()
     },
-    //更改当前页
-    handleCurrentChange(currentPage) {
+    // 更改当前页
+    handleCurrentChange (currentPage) {
       this.queryInfo.pagenum = currentPage
       this.getUserList()
     },
-    //改变用户状态
-    async userStateChange(userInfo) {
+    // 改变用户状态
+    async userStateChange (userInfo) {
       shi
-      //console.log(userInfo)
+      // console.log(userInfo)
       const { data: res } = await this.$http.put(
         `users/${userInfo.id}/state/${userInfo.mg_state}`
       )
-      if (res.meta.status !== 200)
-        return this.$message.error('更新用户状态失败')
+      if (res.meta.status !== 200) { return this.$message.error('更新用户状态失败') }
       this.$message.success('修改用户状态成功')
     },
-    //添加用户
-    addUser() {
+    // 添加用户
+    addUser () {
       this.$refs.ruleForm.validate(async val => {
         if (!val) return
         const { data: res } = await this.$http.post('users', this.userInfo)
-        //console.log(res)
+        // console.log(res)
         if (res.meta.status !== 201) return this.$message.error('添加用户失败')
         this.$message.success('添加用户成功')
         this.dialogVisible = false
         this.getUserList()
       })
-      //this.$refs.ruleForm.resetField()
+      // this.$refs.ruleForm.resetField()
     },
-    //重置表单
-    reSetForm() {
+    // 重置表单
+    reSetForm () {
       this.$refs.ruleForm.resetFields()
     }
   },
-  created() {
+  created () {
     this.getUserList()
   }
 }
